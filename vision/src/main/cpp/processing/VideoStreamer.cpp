c20d6af9d5f53ae6810dc0a64f872703e704fd98
slowl0ris <yuval.friedman@tutanota.com>
2020-02-23 15:13:53 +0000

    camera is working

--- vision/src/main/cpp/processing/VideoStreamer.cpp
+++ vision/src/main/cpp/processing/VideoStreamer.cpp
@@ -7,8 +7,8 @@ VideoStreamer::VideoStreamer(std::string target, int port) {
     " height=(int)480," \
     " framerate=(fraction)30/1" \
     " ! videoconvert ! video/x-raw,format=I420"
-    " ! x264enc tune=zerolatency bitrate=250 threads=1" \
-    " ! rtph264pay config-interval=1 name=pay0 pt=96" \
+    " ! x264enc tune=zerolatency bitrate=250 threads=5" \
+    " ! rtph264pay config-interval=10 name=pay0 pt=96" \
     " ! udpsink host=" + target + " port=" + std::to_string(port) + " ";
     std::cout << pipe_args << std::endl;
     this->_pipeline_out.open(pipe_args, 0, 30, cv::Size(640, 480), true);
